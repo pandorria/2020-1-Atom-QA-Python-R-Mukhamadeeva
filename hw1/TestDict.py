bd693cce07f7f610dfd55823b12b85c6d44cd557
pandorria <61185659+pandorria@users.noreply.github.com>
2020-03-02 00:45:13 +0300

    fixed hw1

---
+++ hw1/TestDict.py
@@ -0,0 +1,59 @@
+import pytest
+
+
+class TestDict:
+
+    def test_dict_1(self):
+        x = {
+            "A": 5,
+            "B": 4,
+            "C": 4,
+            "D1": 4,
+            "D2": 3,
+            "F": 2,
+        }
+        assert x["C"] == 4
+
+    def test_dict_2(self):
+        x = {
+            "A": 5,
+            "B": 4,
+            "C": 4,
+            "D1": 4,
+            "D2": 3,
+            "F": 2,
+        }
+        assert "C" and "D1" in x
+
+    def test_dict_3(self):
+        x = {
+            "A": 5,
+            "B": 4,
+            "C": 4,
+            "D1": 4,
+            "D2": 3,
+            "F": 2,
+        }
+        assert len(x) == 6
+
+    def test_dict_4(self):
+        x = {
+            "A": 5,
+            "B": 4,
+            "C": 4,
+            "D1": 4,
+            "D2": 3,
+            "F": 2,
+        }
+        assert x["D1"] != x["D2"]
+
+    @pytest.mark.parametrize('x', [{
+        "A": 5,
+        "B": 4,
+        "C": 4,
+        "D1": 4,
+        "D2": 3,
+        "F": 2,
+    }, {"B": 4}])
+    def test_dict_5(self, x):
+        assert "E" not in x
---
+++ hw1/TestInt.py
@@ -0,0 +1,28 @@
+import pytest
+import random
+
+
+class TestInt:
+
+    def test_int_1(self):
+        x = 5
+        y = 6
+        assert x < y
+
+    def test_int_2(self):
+        a = 4589
+        assert isinstance(a, int)
+
+    def test_int_3(self):
+        x = random.randrange(1, 5)
+        y = random.randrange(5, 10)
+        assert x != y
+
+    @pytest.mark.parametrize('x', list(range(-5, 7)))
+    def test_int_4(self, x):
+        assert x > -6
+
+    @pytest.mark.parametrize('x', list(range(-2, 2, 2)))
+    @pytest.mark.parametrize('y', list(range(3, 7, 2)))
+    def test_int_4(self, x, y):
+        assert x != y
