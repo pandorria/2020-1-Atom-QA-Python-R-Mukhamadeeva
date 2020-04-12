d062749f8a9a07b231cd1f97e78afaea190aa8e7
pandorria <61185659+pandorria@users.noreply.github.com>
2020-04-12 17:17:36 +0300

    fixed HW2

--- hw2/code/tests/test_ui.py
+++ hw2/code/tests/test_ui.py
@@ -5,38 +5,33 @@ import os
 
 class Test(BaseCase):
     @pytest.mark.UI
-    # @pytest.mark.skip(reason='TEMP')
     def test_login(self):
         self.login_page.login('test.qa20@yandex.ru', 'QWer1@')
         assert "Invalid login or password" not in self.driver.page_source
 
     @pytest.mark.UI
-    # @pytest.mark.skip(reason='TEMP')
     def test_login_negative(self):
         self.login_page.login('test.qa20@yandex.ru', 'vcf3dmkl')
         assert "Invalid login or password" in self.driver.page_source
 
     @pytest.mark.UI
-    # @pytest.mark.skip(reason='TEMP')
     def test_create_advert(self):
         self.advert.login('test.qa20@yandex.ru', 'QWer1@')
         self.advert.click(self.advert.locators.COMPANY_BUTTON)
         self.advert.click(self.advert.locators.NEW_CAMPAIGN, timeout=20)
         self.advert.click(self.advert.locators.AUDIO_ADVERT, timeout=20)
-        self.advert.find(self.advert.locators.ADD_AUDIO).send_keys(os.getcwd() + "/audio.mp3")
+        self.advert.find(self.advert.locators.ADD_AUDIO).send_keys(os.path.dirname(__file__) + "/audio.mp3")
         self.advert.click(self.advert.locators.ADD_ADVERT, timeout=20)
         self.advert.find(self.advert.locators.DONE, timeout=10)
         assert "Создана рекламная кампания" in self.driver.page_source
 
     @pytest.mark.UI
-    # @pytest.mark.skip(reason='TEMP')
     def test_create_segment(self):
         segment_name = "test_create_segment"
         self.segment.create_segment(segment_name)
         assert segment_name in self.driver.page_source
 
     @pytest.mark.UI
-    # @pytest.mark.skip(reason='TEMP')
     def test_delete_segment(self):
         segment_name = "test_delete_segment"
         self.segment.create_segment(segment_name)
